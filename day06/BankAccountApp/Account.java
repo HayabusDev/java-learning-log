4ed30309d1e6e943506b2fac814a032410bea662
HayabusaDev <dashhayabusa1412@gmail.com>
2026-01-29 12:09:59 +0900

    Update Account.java

--- day06/BankAccountApp/Account.java
+++ day06/BankAccountApp/Account.java
@@ -1,1 +1,37 @@
+public class Account {
+    private final int id;
+    private final String userPassword;
+    private double balance;
 
+    Account(int id, String userPassword, double balance){
+        this.id = id;
+        this.userPassword = userPassword;
+        this.balance = balance;
+    }
+
+    public boolean deposit(double depositBalance) {
+        if (depositBalance <= 0){
+            return false;
+        }else {
+            balance = balance + depositBalance;
+            return true;
+        }
+    }
+
+    public boolean withdraw(double withdrawBalance) {
+        if (withdrawBalance <= 0){
+            return false;
+        }else if(balance < withdrawBalance){
+            return false;
+        }else {
+            balance = balance - withdrawBalance;
+            return true;
+        }
+    }
+
+    private String getUserPassword() { return userPassword; }
+
+    public int getId() { return id; }
+
+    public double getBalance(){ return balance;}
+}
