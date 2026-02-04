4ee86e2154e0d4a4755083c82b827292abb3ed64
HayabusaDev <dashhayabusa1412@gmail.com>
2026-02-04 17:00:05 +0900

    Create User.java

---
+++ Day10-member-management-system/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private final String userId;
+
+    public User(String userId){
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+}
