af062d9bc39bfe49917360948cdd15e7fa569dbe
HayabusaDev <dashhayabusa1412@gmail.com>
2026-02-04 17:00:41 +0900

    Delete Day10-member-management-system/main.java

--- Day10-member-management-system/main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Repository repository = new Repository();
-        Service service = new Service(repository);
-        Controller controller = new Controller(service);
-
-        controller.launch();
-    }
-}
