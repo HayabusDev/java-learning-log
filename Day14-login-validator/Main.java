4956ccd5b66f56f1f82d911965452985bfd8bd8d
HayabusaDev <dashhayabusa1412@gmail.com>
2026-02-12 02:35:54 +0900

    Update Main.java

--- Day14-login-validator/Main.java
+++ Day14-login-validator/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
+        Repository repository = new Repository();
+        Validator validator = new Validator();
+        Service service = new Service(repository, validator);
+        Controller controller = new Controller(service);
 
+        controller.launch();
     }
 }
