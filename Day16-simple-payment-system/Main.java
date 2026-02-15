7f72e251b295048f5d1228350de07d27f0a24842
HayabusaDev <dashhayabusa1412@gmail.com>
2026-02-16 04:47:51 +0900

    Update Main.java

--- Day16-simple-payment-system/Main.java
+++ Day16-simple-payment-system/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
 
+        Service service = new Service();
+        PaymentFactory paymentFactory = new PaymentFactory();
+        Controller controller = new Controller(service, paymentFactory);
+
+        controller.launch();
     }
 }
