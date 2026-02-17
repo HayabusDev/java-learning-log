8f7b938abb3e11c23c57449d722f525567faa220
HayabusaDev <dashhayabusa1412@gmail.com>
2026-02-18 03:48:50 +0900

    Create TicketOrder.java

---
+++ Day17-ticket-sales-simulator/src/ticketSalesSimulator/domain/TicketOrder.java
@@ -0,0 +1,50 @@
+package tikectSalesSimulator.domain;
+
+import java.util.UUID;
+
+public class TicketOrder {
+    private final String buyerName;
+    private final UUID orderId;
+    private final UUID eventId;
+    private final int purchaseQuantity;
+    private OrderStatus orderStatus;
+
+    public TicketOrder(String buyerName, UUID orderId, UUID eventId, int purchaseQuantity, OrderStatus orderStatus){
+        this.buyerName = buyerName;
+        this.orderId = orderId;
+        this.eventId = eventId;
+        this.purchaseQuantity = purchaseQuantity;
+        this.orderStatus = orderStatus;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public UUID getEventId() {
+        return eventId;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public int getPurchaseQuantity() {
+        return purchaseQuantity;
+    }
+
+    public boolean canCancel(){
+        return this.orderStatus.canCancel();
+    }
+
+    public void changeToCancelStatus(){
+        if(!canCancel()){
+            return;
+        }
+        this.orderStatus = OrderStatus.CANCELED;
+    }
+}
