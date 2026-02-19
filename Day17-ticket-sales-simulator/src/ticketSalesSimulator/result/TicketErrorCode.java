2a19292dd25fff5e88c5fcea2f3902316ac12d2b
HayabusaDev <dashhayabusa1412@gmail.com>
2026-02-19 10:42:17 +0900

    Update TicketErrorCode.java

--- Day17-ticket-sales-simulator/src/ticketSalesSimulator/result/TicketErrorCode.java
+++ Day17-ticket-sales-simulator/src/ticketSalesSimulator/result/TicketErrorCode.java
@@ -5,9 +5,12 @@ public enum TicketErrorCode implements ErrorCodeLike {
     ALL_SEATS_NOT_POSITIVE("EVENT_002", "最大座席数が0以下です。"),
     REMAIN_SEATS_NEGATIVE("EVENT_003", "残り座席数が負数になっています。"),
     REMAIN_SEATS_BIGGER_ALL_SEATS("EVENT_004", "残り座席数が最大座席数を超えています。"),
+    SALE_STATUS_NULL("EVENT_005", "saleStatus情報がありません。"),
 
     QUANTITY_NOT_POSITIVE("PURCHASE_001", "quantityが0以下です。"),
     BUYER_NAME_EMPTY("PURCHASE_002", "buyerNameが空です。"),
+    ORDER_STATUS_NULL("PURCHASE_003", "orderStatusがありません。"),
+
 
     EVENT_ID_NULL("ID_001", "eventIdがnullです。"),
     TICKET_ORDER_ID_NULL("ID_002", "orderIdがnullです。"),
