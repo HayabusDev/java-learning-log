ca9cfc38767e33adc3c705f061fa2d976196c775
HayabusaDev <dashhayabusa1412@gmail.com>
2026-02-20 08:54:22 +0900

    Update Main.java

--- Day17-ticket-sales-simulator/src/ticketSalesSimulator/Main.java
+++ Day17-ticket-sales-simulator/src/ticketSalesSimulator/Main.java
@@ -1,1 +1,19 @@
+package tikectSalesSimulator;
 
+import tikectSalesSimulator.controller.TicketController;
+import tikectSalesSimulator.repository.EventRepository;
+import tikectSalesSimulator.repository.OrderRepository;
+import tikectSalesSimulator.service.TicketService;
+import tikectSalesSimulator.validator.TicketValidator;
+
+public class Main {
+    public static void main(String[] args){
+        EventRepository eventRepository = new EventRepository();
+        OrderRepository orderRepository = new OrderRepository();
+        TicketValidator ticketValidator = new TicketValidator();
+        TicketService ticketService = new TicketService(eventRepository, orderRepository, ticketValidator);
+        TicketController ticketController = new TicketController(ticketService);
+
+        ticketController.launch();
+    }
+}
