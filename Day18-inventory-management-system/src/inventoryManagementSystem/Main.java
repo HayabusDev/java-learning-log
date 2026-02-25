e342e9e11a3723bebf78670f09016e4dee154f21
HayabusaDev <dashhayabusa1412@gmail.com>
2026-02-26 08:29:03 +0900

    Update Main.java

--- Day18-inventory-management-system/src/inventoryManagementSystem/Main.java
+++ Day18-inventory-management-system/src/inventoryManagementSystem/Main.java
@@ -1,7 +1,17 @@
 package inventoryManagementSystem;
 
+import inventoryManagementSystem.controller.ItemController;
+import inventoryManagementSystem.repository.ItemRepository;
+import inventoryManagementSystem.service.StockService;
+import inventoryManagementSystem.validator.ItemValidator;
+
 public class Main {
     public static void main(String[] args){
-        
+        ItemRepository itemRepository = new ItemRepository();
+        ItemValidator itemValidator = new ItemValidator();
+        StockService stockService = new StockService(itemRepository, itemValidator);
+        ItemController itemController = new ItemController(stockService);
+
+        itemController.launch();
     }
 }
